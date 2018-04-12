e7502a4edfb6723fe4f630f921680b8b154729f7
Spencer Bliven <spencer.bliven@gmail.com>
2018-04-12 15:46:21 +0200

    Refactor viewports DRYly

--- src/colorexplorer/viewport.ts
+++ src/colorexplorer/viewport.ts
@@ -25,29 +25,24 @@
 //}
 
 interface Viewport<C extends Color> {
+    /**
+     * Update the graphics of this viewport.
+     *
+     * Typically called by event handlers listening for changes to the data.
+     */
     update(): void;
 }
 
-class ColorSlice<C extends Color> implements Viewport<C>{
-    xaxis: number;
-    yaxis: number;
-    //setpoint: C;
-    //private axisSpace: ColorMapping<C, Color3>,
-
-    constructor(private model: VisualizerSpace<C>,
-                 private canvas: HTMLCanvasElement,
-                 xaxis?: number, yaxis?: number,
-                public aesthetics?: any) {
-        this.xaxis = xaxis != undefined ? xaxis : 0;
-        this.yaxis = yaxis != undefined ? yaxis : 1;
-        
+abstract class AxisAlignedViewport<C extends Color> implements Viewport<C> {    
+    constructor(protected model: VisualizerSpace<C>,
+                protected canvas: HTMLCanvasElement,
+                public aesthetics?: any) {        
         model.addEventListener("colorselected", this.update.bind(this));
         this.update();
         
         canvas.addEventListener('mousemove', this.moveHandler.bind(this));
         canvas.addEventListener('click', this.clickHandler.bind(this));
     }
-    
 
     update(): void {
         let ctx: CanvasRenderingContext2D | null = this.canvas.getContext("2d");
@@ -90,11 +85,8 @@ class ColorSlice<C extends Color> implements Viewport<C>{
      * @param color
      * @return x, y pixel coordinate
      */
-    posFromColor(color: C): [number, number] | null {
-        let x = Math.floor(color[this.xaxis] * this.canvas.width);
-        let y = Math.floor((1 - color[this.yaxis]) * this.canvas.width);
-        return [x,y];
-    }
+    abstract posFromColor(color: C): [number, number] | null;
+
     /**
      * Converts a pixel coordinate to a visualizationSpace color
      * This is the inverse of posFromColor
@@ -107,12 +99,8 @@ class ColorSlice<C extends Color> implements Viewport<C>{
      * @param y pixel row index in [0, height)
      * @return a color, with axes orthogonal to this slice taken from the selected color
      */
-    colorFromPos(x: number, y: number): C | null {
-        let coord: C = this.model.selection.slice() as C; // clone
-        coord[this.xaxis] = x/this.canvas.width;
-        coord[this.yaxis] = 1-y/this.canvas.height;
-        return coord;
-    }
+    abstract colorFromPos(x: number, y: number): C | null;
+
     /**
      * Handle mouse motion events
      */
@@ -139,68 +127,46 @@ class ColorSlice<C extends Color> implements Viewport<C>{
     }
 }
 
-class ColorStrip<C extends Color> implements Viewport<C> {
+class ColorSlice<C extends Color> extends AxisAlignedViewport<C>{
+    xaxis: number;
+    yaxis: number;
+
+    constructor(model: VisualizerSpace<C>,
+                  canvas: HTMLCanvasElement,
+                 xaxis?: number, yaxis?: number,
+                public aesthetics?: any) {
+        super(model, canvas, aesthetics);
+        this.xaxis = xaxis != undefined ? xaxis : 0;
+        this.yaxis = yaxis != undefined ? yaxis : 1;
+    }
+    
+    posFromColor(color: C): [number, number] | null {
+        let x = Math.floor(color[this.xaxis] * this.canvas.width);
+        let y = Math.floor((1 - color[this.yaxis]) * this.canvas.width);
+        return [x,y];
+    }
+    colorFromPos(x: number, y: number): C | null {
+        let coord: C = this.model.selection.slice() as C; // clone
+        coord[this.xaxis] = x/this.canvas.width;
+        coord[this.yaxis] = 1-y/this.canvas.height;
+        return coord;
+    }
+}
+
+class ColorStrip<C extends Color> extends AxisAlignedViewport<C> {
     axis: number;
     orientation: "vertical" | "horizontal";
     
-    constructor(private model: VisualizerSpace<C>,
-                 private canvas: HTMLCanvasElement,
+    constructor(model: VisualizerSpace<C>,
+                 canvas: HTMLCanvasElement,
                  axis?: number,
                  orientation?: "vertical" | "horizontal",
                  public aesthetics?: any) {
+        super(model, canvas, aesthetics)
         this.axis = axis != undefined ? axis : 0;
         this.orientation = orientation || "horizontal";
-        
-        model.addEventListener("colorselected", this.update.bind(this));
-        this.update();
-
-        canvas.addEventListener('mousemove', this.moveHandler.bind(this));
-        canvas.addEventListener('click', this.clickHandler.bind(this));
     }
 
-    update(): void {
-        let ctx: CanvasRenderingContext2D | null= this.canvas.getContext("2d");
-        if(ctx != null) {
-            let imgdata: ImageData = ctx.getImageData(0,0, this.canvas.width, this.canvas.height);
-            let data: Uint8ClampedArray = imgdata.data;
-            for(let y=0; y< this.canvas.height; y++){
-                for(let x=0; x<this.canvas.width; x++){
-                    let coord = this.colorFromPos(x,y);
-
-                    let color = this.model.getRGB(coord);
-                    
-                    let pos = (y*this.canvas.width + x) * 4;
-                    if( color != null) {
-                        data[pos  ] = color[0]*256;
-                        data[pos+1] = color[1]*256;
-                        data[pos+2] = color[2]*256;
-                        data[pos+3] = 255;
-                    } else {
-                        // null color -> translucent
-                        data[pos  ] = 256;
-                        data[pos+1] = 256;
-                        data[pos+2] = 256;
-                        data[pos+3] = 0;
-                    }
-                }
-            }
-            ctx.putImageData(imgdata, 0,0);
-        }
-    }
-
-    /**
-     * Convert a visualizationSpace color to a pixel coordinate.
-     * This is the inverse of colorFromPos.
-     *
-     * With the default aesthetics, colors start with (0,0,*) in the bottom left
-     * and (1,1,*) in the top right (ignoring axes orthogonal to this slice).
-     * Pixels start with 0,0 in the top left and go to (width, height).
-     *
-     * Depending on the orientation, either the x or y coordinate will be 0.
-     *
-     * @param color
-     * @return x, y pixel coordinate
-     */
     posFromColor(color: C): [number, number] | null {
         let x = 0;
         let y = 0;
@@ -211,18 +177,6 @@ class ColorStrip<C extends Color> implements Viewport<C> {
         }
         return [x,y];
     }
-    /**
-     * Converts a pixel coordinate to a visualizationSpace color
-     * This is the inverse of posFromColor
-     *
-     * Pixels start with 0,0 in the top left and go to (width, height)
-     * With the default aesthetics, colors start with (0,0,*) in the bottom left
-     * and (1,1,*) in the top right.
-     *
-     * @param x pixel column index in [0, width)
-     * @param y pixel row index in [0, height)
-     * @return a color, with axes orthogonal to this slice taken from the selected color
-     */
     colorFromPos(x: number, y: number): C | null {
         let coord: C = this.model.selection.slice() as C; // clone
         if( this.orientation == "horizontal") {
@@ -231,29 +185,5 @@ class ColorStrip<C extends Color> implements Viewport<C> {
             coord[this.axis] = 1 - y/this.canvas.width;
         }
         return coord;
-    }
-    /**
-     * Handle mouse motion events
-     */
-    moveHandler(e: MouseEvent) {
-        if( e.buttons) {
-            let color = this.colorFromPos(e.offsetX, e.offsetY);
-            if( color !== null) {
-                this.model.selection = color;
-            }
-        }
-        //console.log("Moved to " + e.offsetX + ", " + e.offsetY+" button: "+e.button+" buttons: "+e.buttons);
-        
-    }
-    /**
-     * Handle click events
-     */
-    clickHandler(e: MouseEvent) {
-        let color = this.colorFromPos(e.offsetX, e.offsetY);
-        if( color !== null) {
-            this.model.selection = color;
-        }
-        //console.log("clicked at " + e.offsetX + ", " + e.offsetY);
-        //console.log("         = " + color);
     }
 }
